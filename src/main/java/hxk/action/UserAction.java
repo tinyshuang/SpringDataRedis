7f2a7ef26bc4a12fe235c5312889937f5d12f9f8
tinyshuang <distinys@126.com>
2015-12-09 15:09:42 +0800

    add the tx and update the config

--- src/main/java/hxk/action/UserAction.java
+++ src/main/java/hxk/action/UserAction.java
@@ -1,8 +1,13 @@
 package hxk.action;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -32,6 +37,31 @@ public class UserAction {
 	User user = (User) template.opsForHash().get("User", "tinys");
 	System.out.println(user);
 	return "ok";
+    }
+    
+    
+    /**
+     * @description 一个redis的事务操作
+     * 照搬官方文档的..
+     * 也可以在配置文件中的template类添加enableTransactionSupport属性来支持事务	 
+     * @return
+     *2015年12月9日  下午2:40:31
+     *返回类型:String
+     */
+    @RequestMapping("/tx")
+    public @ResponseBody String transactions(){
+	template.execute(new SessionCallback<List<Object>>() {
+	    @Override
+	    public List<Object> execute(RedisOperations operations) throws DataAccessException {
+		operations.multi();
+		operations.opsForSet().add("key", "value1");
+		
+		// This will contain the results of all ops in the transaction
+		return operations.exec();
+	    }
+	    
+	});
+	return "tx";
     }
     
     
